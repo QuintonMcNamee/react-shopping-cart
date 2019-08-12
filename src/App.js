46b17db0326c3a54ce8e88ed7c36294ce186dae0
QuintonStudent <49251721+QuintonStudent@users.noreply.github.com>
2019-08-12 16:20:03 -0400

    step 3 complete

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
-// Context Components
+// Contexts
 import { ProductContext } from './contexts/ProductContext';
 
 function App() {
